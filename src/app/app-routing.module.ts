320ad6c29cf11bcbb7a5c8cf43d86ffb69bfa8ab
Administrator <benoit.routier@semifir.com>
2022-09-14 14:28:50 +0200

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecetteComponent } from './recette/recette.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "/", component: RecetteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
